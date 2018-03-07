273d6b03b64344050908ec45d800b97da770b630
Ragnhildur Rós Gudmundsdóttir <ragnhildurrosgudmundsdottir@Ragnhildurs-MacBook-Pro.local>
2018-03-07 13:14:30 +0000

    test2

--- textUtilities.js
+++ textUtilities.js
@@ -1,3 +1,15 @@
-var exspect = require('chai').expect;
+var expect = require('chai').expect;
 
-expect(true).to.be.true;
+// expect(true).to.be.true;
+
+function titleCase(title) {
+  return title;
+
+}
+
+expect(titleCase('the great mouse detective')).to.be.a('string');
+
+
+
+// 
+// expect(titleCase('the great mouse detective')).to.equal('The Great Mouse Dedective');
